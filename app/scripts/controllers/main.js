c0e62b4f60b3578bc0a2e9de48795d09092c4a51
Grant <grant_guo@dataagility.com>
2015-04-22 09:54:53 +1000

    ng-click added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,23 +7,20 @@
  * # MainCtrl
  * Controller of the mytodoApp
  */
- angular.module('mytodoApp')
- .controller('MainCtrl', function ($scope,$http,$window) {
- 	$scope.urlParts = {};
- 	$scope.urlParts.protocol = "http"; 
- 	$scope.urlParts.domain = "http://198.11.173.181/cgi-bin/GInfo.dll?EmmisTrack&w=blueskyexpress&cno="; 
- 	$scope.urlParts.prefix = "&cmodel=&ntype=0"; 
- 	$scope.json;
- 	$http.get('upload/BlueSky.json').success(function(data) {
- 		$scope.json = data;
- 	});
- 	console.log($scope.json);
- 	$scope.tn = $scope.json.tn;
- 	$scope.url = 'http://198.11.173.181/cgi-bin/GInfo.dll?EmmisTrack&w=blueskyexpress&cno='+'&cmodel=&ntype=0';
- 	$scope.redirectToGoogle = function(){
- 		$window.open($scope.url, '_blank'); 
- 	};
- 	$scope.makeUrl = function() {
- 		return $scope.urlParts.protocol + "://" + $scope.urlParts.domain +$scope.tn+  $scope.urlParts.prefix;
- 	}
- });
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope,$http,$window) {
+  	$scope.json = [];
+     $http.get('upload/BlueSky.json').success(function(data) {
+      $scope.json = data;
+    });
+     console.log('scope.json: '+$scope.json);
+     
+
+     $scope.urlpart1 = 'http://198.11.173.181/cgi-bin/GInfo.dll?EmmisTrack&w=blueskyexpress&cno=';
+     $scope.urlpart2 = '&cmodel=&ntype=0';
+     $scope.redirectToTrackingSite = function(){
+            $window.open($scope.urlpart1+$scope.json.tn+$scope.urlpart2, '_blank'); 
+        };
+       
+
+  });
