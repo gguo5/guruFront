0a7817723da9dbbe0cb98472a21224b90f1279d8
Grant <grant_guo@dataagility.com>
2015-04-22 10:13:31 +1000

    added no cache

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,20 +7,23 @@
  * # MainCtrl
  * Controller of the mytodoApp
  */
-angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope,$http,$window) {
-  	$scope.json = [];
-     $http.get('upload/BlueSky.json').success(function(data) {
-      $scope.json = data;
+ angular.module('mytodoApp')
+ .controller('MainCtrl', function ($scope,$http,$window) {
+
+
+
+    $scope.json = [];
+    $http.get('upload/BlueSky.json').success(function(data) {
+    	$scope.json = data;
     });
-     console.log('scope.json: '+$scope.json);
-     
+    console.log('scope.json: '+$scope.json);
 
-     $scope.urlpart1 = 'http://198.11.173.181/cgi-bin/GInfo.dll?EmmisTrack&w=blueskyexpress&cno=';
-     $scope.urlpart2 = '&cmodel=&ntype=0';
-     $scope.redirectToTrackingSite = function(){
-            $window.open($scope.urlpart1+$scope.json.tn+$scope.urlpart2, '_blank'); 
-        };
-       
 
-  });
+    $scope.urlpart1 = 'http://198.11.173.181/cgi-bin/GInfo.dll?EmmisTrack&w=blueskyexpress&cno=';
+    $scope.urlpart2 = '&cmodel=&ntype=0';
+    $scope.redirectToTrackingSite = function(){
+    	$window.open($scope.urlpart1+$scope.json.tn+$scope.urlpart2, '_blank'); 
+    };
+
+
+});
